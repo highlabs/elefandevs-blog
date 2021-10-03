6b7baa05063b01a5c649eed985d46566c54c2d41
Daniel Oshiro <dobrado@tutanota.com>
2021-10-03 19:48:37 +0100

    feat: post list

---
+++ components/PostList.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <h1>Posts</h1>
+    <div>
+      <ul>
+        <li v-for="post in posts" :key="post.slug">
+          <h2>{{ post.title }}</h2>
+          <p>{{ post.description }}</p>
+          <nuxt-link :to="post.path">Ler artigo</nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    posts: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
