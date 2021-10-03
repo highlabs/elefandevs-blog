0633fe71f536246aee5ab7f0d16532b7e3185948
Daniel Oshiro <dobrado@tutanota.com>
2021-10-03 19:48:43 +0100

    feat: post content

---
+++ pages/artigos/_slug.vue
@@ -0,0 +1,21 @@
+<template>
+  <article>
+    <h1>{{ post.title }}</h1>
+    <h2>{{ post.description }}</h2>
+    <nuxt-content :document="post" />
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content, params }) {
+    const post = await $content('artigos')
+      .where({ slug: params.slug })
+      .fetch()
+
+    return {
+      post: post[0]
+    }
+  }
+}
+</script>
